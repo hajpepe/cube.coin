6914accff680da8d16b5b3b56d6729ac3cb30b14
hajpepe <abolfazlgoldrani@gmail.com>
2025-01-18 05:59:12 -0800

    ss

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient, MongoClientOptions } from "mongodb";
 
-const uri: string = process.env.MONGODB_URI || ""; // Add this in your .env.local
+const uri: string = process.env.MONGODB_URI || "";
 const options: MongoClientOptions = {};
 
 let client: MongoClient;
@@ -11,14 +11,12 @@ if (!process.env.MONGODB_URI) {
 }
 
 if (process.env.NODE_ENV === "development") {
-  // Use a global variable to preserve the client across hot reloads in development
   if (!(global as any)._mongoClientPromise) {
     client = new MongoClient(uri, options);
     (global as any)._mongoClientPromise = client.connect();
   }
   clientPromise = (global as any)._mongoClientPromise;
 } else {
-  // For production, create a new client
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
